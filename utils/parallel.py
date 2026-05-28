a716fde0ddca048a39baff656788400f42726149
= <raullopezdominguez@gmail.com>
2026-05-28 12:52:40 +0200

    cleaning older files and folders

--- utils/parallel.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import Pool
-from tqdm import tqdm
-
-def parallelize(func, args, ncores):
-    with Pool(ncores) as pool:
-        # imap_unordered devuelve un iterador inmediato, permitiendo que tqdm se actualice uno a uno
-        results = list(tqdm(pool.imap_unordered(func, args), total=len(args)))
-    return results
